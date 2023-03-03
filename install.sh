717634ad57396b64ecc18be9bc35259aebad3f78
vlmarch <marchenkovlad@outlook.com>
2023-03-04 00:14:35 +0100

    Add install.sh

---
+++ install.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+DOTFILES_ARCHIVE_DIR=~/dotfiles_archive
+
+FILES_LN="
+    aliases
+    bash_logout
+    bashrc
+    condarc
+    profile
+    zlogout
+    zshrc
+"
+
+REPLACE_FILES_CP=false
+
+FILES_CP="
+    gitconfig
+    gitignore_global
+"
+
+if [ ! -d "$DIRECTORY" ]; then
+    mkdir -pv $DOTFILES_ARCHIVE_DIR
+fi
+
+for file in $FILES_LN $FILES_CP; do
+    if [ -f  ~/.$file ]; then
+        if [ ! -f  $DOTFILES_ARCHIVE_DIR/.$file ]; then
+            mv -v ~/.$file $DOTFILES_ARCHIVE_DIR
+        else
+            echo ERROR! .${file} already exists in ${DOTFILES_ARCHIVE_DIR}
+            exit 1
+        fi
+    fi
+done
+
+for file in $FILES_LN; do
+    path=$(pwd)/.$file
+    ln -sv $path ~/.$file
+done
+
+
+if $REPLACE_FILES_CP; then
+    for file in $FILES_CP; do
+        cp -v ./.$file ~/.$file
+    done
+fi
