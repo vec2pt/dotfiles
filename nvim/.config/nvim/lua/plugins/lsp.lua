88b935d9289026c9b3905df0390feda51fb553bc
vec2pt <68401718+vec2pt@users.noreply.github.com>
2025-02-17 00:54:42 +0100

    Update lspconfig

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -6,22 +6,22 @@ return {
     -- Autocompletion
     {
         'hrsh7th/nvim-cmp',
-        dependencies = {'hrsh7th/cmp-nvim-lsp-signature-help', "hrsh7th/cmp-buffer", "hrsh7th/cmp-path"},
+        dependencies = { 'hrsh7th/cmp-nvim-lsp-signature-help', "hrsh7th/cmp-buffer", "hrsh7th/cmp-path" },
         event = 'InsertEnter',
         config = function()
             local cmp = require('cmp')
 
             cmp.setup({
-                sources = {{name = 'nvim_lsp'}, {name = 'nvim_lsp_signature_help'}, {name = 'buffer'}, {name = 'path'}},
+                sources = { { name = 'nvim_lsp' }, { name = 'nvim_lsp_signature_help' }, { name = 'buffer' }, { name = 'path' } },
                 mapping = cmp.mapping.preset.insert({
                     -- ['<C-Space>'] = cmp.mapping.complete(),
                     -- ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                     -- ['<C-d>'] = cmp.mapping.scroll_docs(4),
-                    ["<CR>"] = cmp.mapping.confirm({select = true}),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
                     ["<Tab>"] = cmp.mapping.select_next_item(),
                     ["<S-Tab>"] = cmp.mapping.select_prev_item()
                 }),
-                snippet = {expand = function(args) vim.snippet.expand(args.body) end}
+                snippet = { expand = function(args) vim.snippet.expand(args.body) end }
             })
         end
     },
@@ -30,8 +30,8 @@ return {
     {
         'neovim/nvim-lspconfig',
         cmd = 'LspInfo',
-        event = {'BufReadPre', 'BufNewFile'},
-        dependencies = {{'hrsh7th/cmp-nvim-lsp'}},
+        event = { 'BufReadPre', 'BufNewFile' },
+        dependencies = { { 'hrsh7th/cmp-nvim-lsp' } },
         init = function()
             -- Reserve a space in the gutter
             -- This will avoid an annoying layout shift in the screen
@@ -43,14 +43,14 @@ return {
             -- Add cmp_nvim_lsp capabilities settings to lspconfig
             -- This should be executed before you configure any language server
             lsp_defaults.capabilities = vim.tbl_deep_extend('force', lsp_defaults.capabilities,
-                                                            require('cmp_nvim_lsp').default_capabilities())
+                require('cmp_nvim_lsp').default_capabilities())
 
             -- LspAttach is where you enable features that only work
             -- if there is a language server active in the file
             vim.api.nvim_create_autocmd('LspAttach', {
                 desc = 'LSP actions',
                 callback = function(event)
-                    local opts = {buffer = event.buf}
+                    local opts = { buffer = event.buf }
 
                     vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
                     vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
@@ -60,7 +60,7 @@ return {
                     vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
                     vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
                     vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-                    vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                    vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
                     vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
                 end
             })
@@ -73,7 +73,34 @@ return {
             require('lspconfig').pyright.setup({})
 
             -- Lua
-            require('lspconfig').lua_ls.setup({})
+            -- require('lspconfig').lua_ls.setup({})
+            -- https://github.com/neovim/neovim/issues/21686#issuecomment-1522446128
+            require('lspconfig').lua_ls.setup {
+                settings = {
+                    Lua = {
+                        runtime = {
+                            -- Tell the language server which version of Lua you're using
+                            -- (most likely LuaJIT in the case of Neovim)
+                            version = 'LuaJIT',
+                        },
+                        diagnostics = {
+                            -- Get the language server to recognize the `vim` global
+                            globals = {
+                                'vim',
+                                'require'
+                            },
+                        },
+                        workspace = {
+                            -- Make the server aware of Neovim runtime files
+                            library = vim.api.nvim_get_runtime_file("", true),
+                        },
+                        -- Do not send telemetry data containing a randomized but unique identifier
+                        telemetry = {
+                            enable = false,
+                        },
+                    },
+                },
+            }
 
             -- HTML/CSS/JSON/ESLint
             require('lspconfig').html.setup {}
@@ -81,13 +108,13 @@ return {
             require('lspconfig').jsonls.setup {}
 
             -- Rust
-            require('lspconfig').rls.setup {}
+            require('lspconfig').rust_analyzer.setup {}
 
-            -- C/C++/Objective-C
-            require('lspconfig').ccls.setup {}
+            -- C/C++
+            require('lspconfig').clangd.setup {}
 
             -- YAML
-            require'lspconfig'.yamlls.setup {}
+            require 'lspconfig'.yamlls.setup {}
         end
     }
 }
