b831faa7e4acb9da678c42ecbb11812b2dcbe306
vec2pt <68401718+vec2pt@users.noreply.github.com>
2025-07-20 21:09:26 +0200

    diagnostic

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -145,8 +145,8 @@ return {
                     -- See :help vim.diagnostic.Opts
                     vim.diagnostic.config {
                         severity_sort = true,
-                        float = { border = "rounded", source = "if_many" },
-                        underline = { severity = vim.diagnostic.severity.ERROR },
+                        float = { border = "solid", source = "if_many" },
+                        -- underline = { severity = vim.diagnostic.severity.ERROR },
                         signs = {
                             text = {
                                 [vim.diagnostic.severity.ERROR] = "󰅚 ",
@@ -155,19 +155,20 @@ return {
                                 [vim.diagnostic.severity.HINT] = "󰌶 ",
                             },
                         } or {},
-                        virtual_text = {
-                            source = "if_many",
-                            spacing = 2,
-                            format = function(diagnostic)
-                                local diagnostic_message = {
-                                    [vim.diagnostic.severity.ERROR] = diagnostic.message,
-                                    [vim.diagnostic.severity.WARN] = diagnostic.message,
-                                    [vim.diagnostic.severity.INFO] = diagnostic.message,
-                                    [vim.diagnostic.severity.HINT] = diagnostic.message,
-                                }
-                                return diagnostic_message[diagnostic.severity]
-                            end,
-                        },
+                        virtual_text = false,
+                        -- virtual_text = {
+                        --     source = "if_many",
+                        --     spacing = 2,
+                        --     format = function(diagnostic)
+                        --         local diagnostic_message = {
+                        --             [vim.diagnostic.severity.ERROR] = diagnostic.message,
+                        --             [vim.diagnostic.severity.WARN] = diagnostic.message,
+                        --             [vim.diagnostic.severity.INFO] = diagnostic.message,
+                        --             [vim.diagnostic.severity.HINT] = diagnostic.message,
+                        --         }
+                        --         return diagnostic_message[diagnostic.severity]
+                        --     end,
+                        -- },
                     }
                 end,
             })
